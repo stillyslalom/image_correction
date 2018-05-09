c53f609f59eda8b0d73f88bad5eaec77391c89fd
Alex Ames <aames@wisc.edu>
2018-05-09 15:56:56 -0500

    improved writevideo script

--- io/writevideo.jl
+++ io/writevideo.jl
@@ -1,31 +1,24 @@
-using Images
+using Images, ProgressMeter.@showprogress
 
-function writevideo(fname, imgstack;
-                    overwrite=true, framerate=30, compat_mode=false)
-    # Input validation
-    length(size(imgstack)) == 3 || error("input image must have three dimensions")
-    if !(eltype(imgstack) <: Color)
-        error("element type of input array is $(eltype(imgstack));
-         needs to be RGB or Gray")
-     end
+function writevideo(fname, imgstack::Array{<:Color,3};
+                    overwrite=true, fps=30::UInt, options=``)
+    ow = overwrite ? `-y` : `-n`
+    h, w, nframes = size(imgstack)
 
-    # Translate inputs to command line args
-    ow = overwrite ? "-y" : ""
-    w, h, nframes = size(imgstack)
-
-    if eltype(imgstack) <: RGB
-    open(`ffmpeg $ow -f rawvideo -pix_fmt rgb24 -s:v $(w)x$(h)
-            -r $framerate -i pipe:0 -vf "transpose=0" $fname`, "w") do out
-        for i = 1:nframes
-            write(out, convert.(RGB{N0f8}, imgstack[:,:,i]))
-        end
-    end
-    else
-        open(`ffmpeg $ow -f rawvideo -pix_fmt gray -s:v $(w)x$(h)
-            -r $framerate -i pipe:0 -vf "transpose=0" $fname`, "w") do out
-            for i = 1:nframes
-                write(out, convert.(Gray{N0f8}, imgstack[:,:,i]))
-            end
+    open(`ffmpeg
+            -loglevel warning
+            $ow
+            -f rawvideo
+            -pix_fmt rgb24
+            -s:v $(h)x$(w)
+            -r $fps
+            -i pipe:0
+            $options
+            -vf "transpose=0"
+            -pix_fmt yuv420p
+            $fname`, "w") do out
+        @showprogress .1 "Writing video \t" for i = 1:nframes
+            write(out, convert.(RGB{N0f8}, clamp01.(imgstack[:,:,i])))
         end
     end
 end
